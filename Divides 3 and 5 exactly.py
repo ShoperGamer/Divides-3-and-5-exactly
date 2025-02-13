66edf63c3a5d7e3d7d9314836f319aa66aa6d0b2
ShoperGamer <gakungza123@gmail.com>
2025-02-13 12:45:50 +0700

    Update Divides 3 and 5 exactly.py

--- Divides 3 and 5 exactly.py
+++ Divides 3 and 5 exactly.py
@@ -1,1 +1,18 @@
+N = int(input("number(จำนวนตัวเลข) : "))
+
+x = []
+for i in range (N): #วนลูปตามจำนวนเลข
+  num = int(input("number : "))
+  x.append(num) #ตรวจสอบเงื่อนไข
+
+count3 = 0 #เอาไว้หาร 3
+count5 = 0 #เอาไว้หาร 5
+
+for i in x:
+  if i % 3 == 0:
+    count3 += 1
+  if i % 5 == 0:
+    count5 += 1
+
+  print("หาร 3 ลง :",count3,"หาร 5 ลง :",count5)
 
